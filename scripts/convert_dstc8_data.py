5e7354b9550efc0632c5f863f3aba5564178865f
Sanyuan-Chen <sychen@g.harvard.edu>
2019-08-18 23:11:20 +0800

    fixed some bugs

--- scripts/convert_dstc8_data.py
+++ scripts/convert_dstc8_data.py
@@ -16,12 +16,12 @@ def get_context(dialog):
             context += msg['utterance'] + ' '# " __eou__ "
             speaker = msg['speaker']
         elif speaker != msg['speaker']:
-            context = context.strip() + " \t" + msg['utterance'] + ' '#" __eou__ "
+            context = context.strip() + "\t" + msg['utterance'] + ' '#" __eou__ "
             speaker = msg['speaker']
         else:
             context += msg['utterance'] + ' '#" __eou__ "
 
-    context = context.strip() + " \t"
+    context = context.strip() + "\t"
     return context
 
 def create_train_file(train_file, train_file_out, opt):
@@ -34,7 +34,7 @@ def create_train_file(train_file, train_file_out, opt):
     for index, entry in enumerate(json_data):
         # row = str(index+1) + "\t"
         context = get_context(entry)
-        row = context #+ " \t"
+        row = context #+ "\t"
 
         if len(entry['options-for-correct-answers']) == 0:
             correct_answer = {}
@@ -45,7 +45,7 @@ def create_train_file(train_file, train_file_out, opt):
             target_id = correct_answer['candidate-id']
         answer = correct_answer['utterance'] #+ " __eou__ "
         answer = answer.strip()
-        correct_answer_row = '1 \t' + row + answer
+        correct_answer_row = '1\t' + row + answer
         positive_samples_count += 1
         train_file_op.write(correct_answer_row.replace("\n", "") + "\n")
 
@@ -56,14 +56,16 @@ def create_train_file(train_file, train_file_out, opt):
             answer = utterance['utterance'] #+ " __eou__ "
             answer = answer.strip()
             negative_answers.append(answer)
-        answer = "None"# __eou__"
-        negative_answers.append(answer)
+        print('train', target_id, len(negative_answers))
+        if target_id != "NONE":
+            answer = "None"# __eou__"
+            negative_answers.append(answer)
 
         negative_samples = random.sample(negative_answers, opt.neg_pos_ratio)
         assert len(negative_samples) == opt.neg_pos_ratio
 
         for i in range(len(negative_samples)):
-            negative_answer_row = '0 \t' + row + negative_samples[i]
+            negative_answer_row = '0\t' + row + negative_samples[i]
             negative_samples_count += 1
             train_file_op.write(negative_answer_row.replace("\n", "") + "\n")
 
@@ -72,52 +74,55 @@ def create_train_file(train_file, train_file_out, opt):
     print("Train - Negative samples count - {}".format(negative_samples_count))
     train_file_op.close()
 
-# def create_dev_file(dev_file, dev_file_out, answers):
-#     dev_file_op = open(dev_file_out, "w")
-#     positive_samples_count = 0
-#     negative_samples_count = 0
-#
-#     dev_data_handle = open(dev_file, 'rb')
-#     json_data = ijson.items(dev_data_handle, 'item')
-#     for index, entry in enumerate(json_data):
-#         row = str(index+1) + "\t"
-#         context = get_context(entry)
-#         row += context + "\t"
-#
-#         if len(entry['options-for-correct-answers']) == 0:
-#             correct_answer = {}
-#             correct_answer['utterance'] = "None"
-#             target_id = "NONE"
-#         else:
-#             correct_answer = entry['options-for-correct-answers'][0]
-#             target_id = correct_answer['candidate-id']
-#         answer = correct_answer['utterance'] + " __eou__ "
-#         answer = answer.strip()
-#         row += str(answers[answer] + 1) + "\t"
-#         positive_samples_count += 1
-#
-#         negative_answers = []
-#         for i, utterance in enumerate(entry['options-for-next']):
-#             if utterance['candidate-id'] == target_id:
-#                 continue
-#             answer = utterance['utterance'] + " __eou__ "
-#             answer = answer.strip()
-#             negative_answers.append(str(answers[answer] + 1))
-#             negative_samples_count += 1
-#
-#         if len(negative_answers) < 100:
-#             answer = "None __eou__"
-#             negative_answers.append(str(answers[answer] + 1))
-#             negative_samples_count += 1
-#
-#         negative_answers = "|".join(negative_answers)
-#         row += negative_answers + "\t"
-#         dev_file_op.write(row.replace("\n", "") + "\n")
-#
-#     print("Saved dev data to {}".format(dev_file_out))
-#     print("Dev - Positive samples count - {}".format(positive_samples_count))
-#     print("Dev - Negative samples count - {}".format(negative_samples_count))
-#     dev_file_op.close()
+
+def create_dev_file(dev_file, dev_file_out):
+    dev_file_op = open(dev_file_out, "w")
+    positive_samples_count = 0
+    negative_samples_count = 0
+
+    dev_data_handle = open(dev_file, 'rb')
+    json_data = ijson.items(dev_data_handle, 'item')
+    for index, entry in enumerate(json_data):
+        # row = str(index+1) + "\t"
+        context = get_context(entry)
+        row = context #+ "\t"
+
+        if len(entry['options-for-correct-answers']) == 0:
+            correct_answer = {}
+            correct_answer['utterance'] = "None"
+            target_id = "NONE"
+        else:
+            correct_answer = entry['options-for-correct-answers'][0]
+            target_id = correct_answer['candidate-id']
+        answer = correct_answer['utterance'] #+ " __eou__ "
+        answer = answer.strip()
+        correct_answer_row = '1\t' + row + answer
+        positive_samples_count += 1
+        dev_file_op.write(correct_answer_row.replace("\n", "") + "\n")
+
+        negative_answers = []
+        for i, utterance in enumerate(entry['options-for-next']):
+            if utterance['candidate-id'] == target_id:
+                continue
+            answer = utterance['utterance'] #+ " __eou__ "
+            answer = answer.strip()
+            negative_answers.append(answer)
+        print('valid', target_id, len(negative_answers))
+        if target_id != "NONE":
+            answer = "None"# __eou__"
+            negative_answers.append(answer)
+
+        negative_samples = negative_answers
+
+        for i in range(len(negative_samples)):
+            negative_answer_row = '0\t' + row + negative_samples[i]
+            negative_samples_count += 1
+            dev_file_op.write(negative_answer_row.replace("\n", "") + "\n")
+
+    print("Saved valid data to {}".format(dev_file_out))
+    print("Valid - Positive samples count - {}".format(positive_samples_count))
+    print("Valid - Negative samples count - {}".format(negative_samples_count))
+    dev_file_op.close()
 
 
 def create_test_file(test_file, test_file_out):
@@ -152,17 +157,17 @@ def create_test_file(test_file, test_file_out):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-i', '--train_in', type=str, default="users5/sychen/DSTC8_DATA/Task_1/ubuntu/task-1.ubuntu.train.json",
+    parser.add_argument('-i', '--train_in', type=str, default="/users5/sychen/DSTC8_DATA/Task_1/ubuntu/task-1.ubuntu.train.json",
                            help="Path to input data file")
-    parser.add_argument('-o', '--train_out', type=str, default="users5/sychen/pinkcom/data/train.txt",
+    parser.add_argument('-o', '--train_out', type=str, default="/users5/sychen/pinkcom/data/train.txt",
                            help="Path to output train file")
-    parser.add_argument('-di', '--dev_in', type=str, default="users5/sychen/DSTC8_DATA/Task_1/ubuntu/task-1.ubuntu.dev.json",
+    parser.add_argument('-di', '--dev_in', type=str, default="/users5/sychen/DSTC8_DATA/Task_1/ubuntu/task-1.ubuntu.dev.json",
                            help="Path to dev data file")
-    parser.add_argument('-do', '--dev_out', type=str, default="users5/sychen/pinkcom/data/dev.txt",
+    parser.add_argument('-do', '--dev_out', type=str, default="/users5/sychen/pinkcom/data/valid.txt",
                            help="Path to output dev file")
-    parser.add_argument('-ti', '--test_in', type=str, default="users5/sychen/DSTC8_DATA/Task_1/ubuntu/task-1.ubuntu.test.json",
+    parser.add_argument('-ti', '--test_in', type=str, default="/users5/sychen/DSTC8_DATA/Task_1/ubuntu/task-1.ubuntu.test.json",
                            help="Path to test data file")
-    parser.add_argument('-to', '--test_out', type=str, default="users5/sychen/pinkcom/data/test.txt",
+    parser.add_argument('-to', '--test_out', type=str, default="/users5/sychen/pinkcom/data/test.txt",
                            help="Path to output test file")
     #
     # parser.add_argument('-af', '--answers_file', type=str, default="./ans.txt",
@@ -183,7 +188,7 @@ if __name__ == "__main__":
     dev_file = os.path.join(opt.dev_in)
     #test_file = os.path.join(opt.test_in)
 
-    answers_file = os.path.join(opt.answers_file)
+    #answers_file = os.path.join(opt.answers_file)
     #test_answers_file = os.path.join(opt.test_answers_file)
 
     train_file_out = os.path.join(opt.train_out)
@@ -212,7 +217,7 @@ if __name__ == "__main__":
     # Create train txt file
     create_train_file(train_file, train_file_out, opt)
     # Create dev txt file
-    create_train_file(dev_file, dev_file_out, opt)
+    create_dev_file(dev_file, dev_file_out)
 
     ## Once we have the test_set, we need to add test_file here too!
     # Create test txt file
